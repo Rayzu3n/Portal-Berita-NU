fb8c45402cd3cf1ee628adb7a697324366d8f908
Rayzu3n <drasya040@gmail.com>
2026-09-01 13:07:08 +0700

    feat: route public news pages

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\NewsController;
 use App\Http\Controllers\Admin\ResidentController;
+use App\Http\Controllers\PublicNewsController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', [PublicNewsController::class, 'home'])->name('home');
+Route::get('/berita/{news:slug}', [PublicNewsController::class, 'show'])->name('public.news.show');
 
 Route::middleware('auth')->prefix('admin')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])
